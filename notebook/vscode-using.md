b3f6fa24014be0455648e76063c883af7779e87b
wuhaozhou <wuhao5436@gmail.com>
2019-05-07 14:59:37 +0800

    vs-using

---
+++ notebook/vscode-using.md
@@ -0,0 +1,12 @@
+* vscode 使用
+   * 插件
+   
+   名称 | 功能 
+   --- | ---
+  Path Intellisence | 自动补全路径 
+    HTML Snippets | html 标签 
+    ES7 React/Redux/RN | 智能语法提示 快捷操作
+    EditorConfig for VS Code | 代码格式化
+    GitLens | git 工具，查看修改记录
+    Markdown Preview Enhanced | 配合编写Markdown文档
+    Document This | 注释
--- react/simple/src/components/content/index.js
+++ react/simple/src/components/content/index.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react'
 import { Breadcrumb } from 'antd';
 import {Link, Route} from 'react-router-dom';
 import Example from '../detail';
-import { Column, Table } from 'react-virtualized';
-
+import { Table, Button } from 'antd';
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Age',
+    dataIndex: 'age',
+  }, {
+    title: 'Address',
+    dataIndex: 'address',
+  }];
+  const data = [];
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 export default class Content extends Component {
-   
+    state = {
+        selectedRowKeys: [], // Check here to configure the default column
+        loading: false,
+      };
+      start = () => {
+        this.setState({ loading: true });
+        // ajax request after empty completing
+        setTimeout(() => {
+          this.setState({
+            selectedRowKeys: [],
+            loading: false,
+          });
+        }, 1000);
+      }
+    
     render() {
+        const { loading, selectedRowKeys } = this.state;
+        const rowSelection = {
+            // onChange: (selectedRowKeys, selectedRows)=>{console.log('selectedRowKeys, selectedRows', selectedRowKeys, selectedRows)},
+            onSelect: (record, selected, selectedRows, nativeEvent) =>{console.log('record, selected, selectedRows', record, selected, selectedRows)}
+          };
         return (
             <div style={Object.assign({}, this.props.style)}>
                 <Breadcrumb>
@@ -15,7 +52,8 @@ export default class Content extends Component {
                     </Breadcrumb.Item>
                     <Breadcrumb.Item><a href="">文章1</a></Breadcrumb.Item>
                 </Breadcrumb>
-             <Route path='/detail/:id' component={Example}></Route>
+             {/* <Route path='/detail/:id' component={Example}></Route> */}
+             <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
              </div>
         )
     }
